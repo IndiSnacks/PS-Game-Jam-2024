421cbdf49a52dd576e7b4ee0f7ddd4d853a51bac
jwbarkes <jbarkes@ucsd.edu>
2024-07-20 15:16:46 -0700

    Combination logic
    Added base logic to combine potions to create the orange, green, and purple variants

--- Scripts/potion.gd
+++ Scripts/potion.gd
@@ -3,12 +3,13 @@ extends Resource
 class_name Potion
 
 # Define the properties of a potion
+# Potion names will just be there respective color i.e. red for health
 var name: String
 var health_boost: int = 0
 var stamina_boost: int = 0
+var slowing_aura: bool = false
 var healing_aura: bool = false
 var grass_defense: bool = false
-var slowing_aura: bool = false
 var invisibility: bool = false
 
 # Method to apply the potion effects to a monster
@@ -19,3 +20,16 @@ func apply_to(monster):
 	monster.grass = grass_defense
 	monster.slowing = slowing_aura
 	monster.invis = invisibility
+
+func combine_potions(potion1, potion2):
+	var combined_pot = Potion.new()
+	if (potion1.name == 'red' and potion2.name == 'yellow') or (potion1.name == 'yellow' and potion2.name == 'red'): 
+		combined_pot.name = 'orange'
+		combined_pot.healing_aura = true
+	if (potion1.name == 'yellow' and potion2.name == 'blue') or (potion1.name == 'blue' and potion2.name == 'yellow'):
+		combined_pot.name = 'green'
+		combined_pot.grass_defense = true
+	if (potion1.name == 'red' and potion2.name == 'blue') or (potion1.name == 'blue' and potion2.name == 'red'):
+		combined_pot.name = 'purple'
+		combined_pot.invisibility = true
+	return combined_pot
