765080b610596e340e34c1a5ca5349bfb9859aaf
Sahil Gathe <sahilsankur@gmail.com>
2024-07-20 22:09:12 -0700

    feat: added shadow health and knight

--- Scripts/spawning_manager.gd
+++ Scripts/spawning_manager.gd
@@ -1,14 +1,30 @@
 extends Node
 
+@export var num_shadows = 100
+
 var path : Path2D
 var pathfollow2D : PathFollow2D
+var text : TextEdit
 
 func _ready():
 	path = get_parent().get_node("Path2D")
+	text = get_node("TextEdit")
+	text.text = str(num_shadows)
 
 func _on_button_button_down():
-	spawn_shadow()
+	if(num_shadows > 0):
+		spawn_shadow()
+		num_shadows -= 1
+		set_text()
+	else:
+		text.text = "no more shadows!"
+		
 
 func spawn_shadow():
 	pathfollow2D = preload("res://Components/shadow_monster.tscn").instantiate()
 	path.add_child(pathfollow2D)
+
+func set_text():
+	text.text = str(num_shadows)
+
+
