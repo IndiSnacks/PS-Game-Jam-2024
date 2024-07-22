49941272a3790279fbf8b01e6d0bae8c4258f72d
jwbarkes <jbarkes@ucsd.edu>
2024-07-21 21:48:38 -0700

    Updated Priest
    Updated priest charge

--- Scripts/damage_dealer.gd
+++ Scripts/damage_dealer.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 @export var damage_speed = 0.5
 @export var damage_amount = 0.0
-@export var charge_increment = 0.1
+@export var charge_increment = 0.0
 @export var is_aoe = false
 @export var is_charge = false
 
@@ -15,29 +15,31 @@ func _ready():
 
 func _process(delta):
 	timer += delta
-	if is_charge and charge < 100:
-		charge += charge_increment
+	if is_charge:
+		if charge < 100:
+			print('CHARGE: %f', charge)
+			charge += charge_increment
 	if(timer >= damage_speed): 
 		timer = 0.0
 		if(!is_aoe): #damges the first target if not aoe
-			if not is_charge:
-				if(shadow_queue.size() > 0):
+			if(shadow_queue.size() > 0):
+				if is_charge:
+					if charge >= 100:
+						var target = shadow_queue[0]
+						target.take_damage(damage_amount)
+						charge = 0
+				else:
 					var target = shadow_queue[0]
 					target.take_damage(damage_amount)
-			else:
-				if(shadow_queue.size() > 0) and charge >= 100:
-					var target = shadow_queue[0]
-					target.take_damage(damage_amount)
-					charge = 0
 		if(is_aoe): #damages all in raduis if aoe
-			if not is_charge:
-				for shadow in shadow_queue:
-					shadow.take_damage(damage_amount)
-			else:
+			if is_charge:
 				if charge >= 100:
 					for shadow in shadow_queue:
 						shadow.take_damage(damage_amount)
 					charge = 0
+			else:
+				for shadow in shadow_queue:
+					shadow.take_damage(damage_amount)
 
 # Put all the nodes the area into a queue
 # Do tick damage to the top node
@@ -45,10 +47,8 @@ func _process(delta):
 # Pop the node if it leaves the area
 func _on_area_entered(area):
 	if(area.is_in_group("shadow")):
-		print('ADDED SHADOW')
 		shadow_queue.append(area)
 
 func _on_area_exited(area):
 	if(area.is_in_group("shadow")):
-		print('BYE SHADOW')
 		shadow_queue.erase(area)
